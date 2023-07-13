1797e44fe4a022baeb0afc56c40718041b34bf74
MohdNadeem <codewithnadeem@gmail.com>
2023-07-13 18:41:49 +0530

    added friend component

--- src/App.jsx
+++ src/App.jsx
@@ -92,3 +92,34 @@ function FriendList({ friends, onSelection, selectedFriend }) {
     </ul>
   );
 }
+
+export default App;
+
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={friend.image} alt="image" />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {" "}
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className=" "> You and {friend.name} are even</p>
+      )}
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
+    </li>
+  );
+}
