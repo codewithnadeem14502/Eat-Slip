97c301df9d308c9b97126eda6d2ef5c6626b4570
MohdNadeem <codewithnadeem@gmail.com>
2023-07-13 18:40:53 +0530

    add FriendList component

--- src/App.jsx
+++ src/App.jsx
@@ -78,3 +78,17 @@ function App() {
     </div>
   );
 }
+function FriendList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
